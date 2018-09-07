b0385589d6b44ed4740822228dd6ccfa2c0fc531
topus009 <sj_89@mail.ru>
2018-09-07 07:24:19 +0300

    megafon_004 - add some routes

--- src/components/Megafon.js
+++ src/components/Megafon.js
@@ -2,11 +2,21 @@ import React from 'react';
 import {observer, inject} from 'mobx-react';
 // import {lifecycle} from 'recompose';
 import _ from 'lodash';
+import {Route, IndexRoute} from 'react-router'
 
 function Megafon(props) {
   console.warn({props});
   return (
-    <div className='wrapper'/>
+    <div className='wrapper'>
+      <Route exact path='/' component={Home}/>
+      <Route path='/user'>
+          <IndexRoute component={User}/>
+          <Route path=':userId' components={UserInfo}/>
+          <Route path=':userId/edit' components={UserEditForm}/>
+      </Route>
+      <Route path='/adduser' component={UserAddForm}/>
+      <Route path='/contacts' component={Contacts}/>
+    </div>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import {AppContainer} from 'react-hot-loader';
 import {Provider} from 'mobx-react';
 import createBrowserHistory from 'history/createBrowserHistory';
 import {RouterStore, syncHistoryWithStore} from 'mobx-react-router';
-import { Router } from 'react-router';
+import {Router} from 'react-router';
 import Megafon from './components/Megafon';
 import AppStore from './Store';
 
