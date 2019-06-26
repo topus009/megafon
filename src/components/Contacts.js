5805431463d51b51a37af790a03c8d6235989d87
topolov.s <topolov.s@ad1team.ru>
2019-06-26 22:19:49 +0300

    add eslint react-hooks

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,65 +1,45 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import Form from '../common/Form';
 import config from '../../config.local';
-import {getUsers} from '../actions/AppActions';
+import { getUsers } from '../actions/AppActions';
 import UserListItem from './UserListItem';
 
-class Contacts extends Component {
-    componentDidMount() {
-        const {getUsers} = this.props;
-        getUsers();
-    }
-    handleClose = () => {
-        const {history} = this.props;
-        history.push({
-            pathname: `${config.basename}/`,
-            query: {},
-            state: null
-        });
-    }
-    render() {
-        const {
-            users,
-            loading
-        } = this.props;
-        return (
-            <Form
-                onClose={this.handleClose}
-                title='Контакты'
-            >
-                <div className='content'>
-                    {
-                        !loading &&
-                            users.map((item, key) =>
-                                <UserListItem
-                                    key={key}
-                                    userId={item._id}
-                                    history={this.props.history}
-                                />
-                            )
-                    }
-                </div>
-            </Form>
-        );
-    }
-}
+const Contacts = ({ getUsers, users, loading, history }) => {
+  const handleClose = () => {
+    history.push({
+      pathname: `${config.basename}/`,
+      query: {},
+      state: null,
+    });
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <Form onClose={handleClose} title="Контакты">
+      <div className="content">
+        {!loading && users.map(item => <UserListItem key={item._id} userId={item._id} history={history} />)}
+      </div>
+    </Form>
+  );
+};
 
-const mapStateToProps = ({app}) => {
-    const {
-        users,
-        loading
-    } = app;
-    return {
-        users,
-        loading
-    };
-}
+const mapStateToProps = ({ app }) => {
+  const { users, loading } = app;
+  return {
+    users,
+    loading,
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getUsers: () => dispatch(getUsers())
-    };
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    getUsers: () => dispatch(getUsers()),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Contacts);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Contacts);
