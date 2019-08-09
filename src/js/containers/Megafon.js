1d27d95533cc316df0d0b2e69b50220c257ebe6b
topolov.s <topolov.s@ad1team.ru>
2019-08-09 16:24:59 +0300

    shitching theme state

--- src/js/containers/Megafon.js
+++ src/js/containers/Megafon.js
@@ -1,28 +1,20 @@
 import React, { Fragment } from 'react';
-import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createBrowserHistory } from 'history';
 import AppRouter from '../routes';
 import configureStore from '../store';
-import Menu from '../components/Menu';
 import IntlProvider from '../hoc/IntlProvider';
 import { defaultLocale } from '../config/translation';
 
 import '../../styles/base/_main.sass';
 
-const history = createBrowserHistory();
 const Store = configureStore();
-
 const locale = Store.getState()?.inlt?.locale || defaultLocale;
 
 const Megafon = () => {
   return (
     <Provider store={Store}>
       <IntlProvider key={locale} locale={locale} textComponent={Fragment}>
-        <Router history={history}>
-          <Menu />
-          <AppRouter />
-        </Router>
+        <AppRouter />
       </IntlProvider>
     </Provider>
   );
--- src/js/routes/index.js
+++ src/js/routes/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import Helmet from 'react-helmet';
+import { connect } from 'react-redux';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import Menu from '../components/Menu';
 import UserForm from '../components/UserForm';
 import Contacts from '../components/Contacts';
 import Home from '../components/Home';
+import { defaultTheme } from '../config/theme';
 
-const Routes = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/contacts" component={Contacts} />
-    <Route exact path="/user/:userId" component={UserForm} />
-    <Route exact path="/user/:userId/edit" component={UserForm} />
-    <Route exact path="/adduser" component={UserForm} />
-  </Switch>
+const history = createBrowserHistory();
+
+const AppRouter = ({ currentTheme }) => (
+  <Router history={history}>
+    <Helmet htmlAttributes={{ class: currentTheme ? `theme-${currentTheme}` : `theme-${defaultTheme}` }} />
+    <Menu />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/contacts" component={Contacts} />
+      <Route exact path="/user/:userId" component={UserForm} />
+      <Route exact path="/user/:userId/edit" component={UserForm} />
+      <Route exact path="/adduser" component={UserForm} />
+    </Switch>
+  </Router>
 );
-export default Routes;
+
+const mapStateToProps = ({ theme: { currentTheme } }) => ({
+  currentTheme,
+});
+
+export default connect(mapStateToProps)(AppRouter);
