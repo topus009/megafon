896ab43dcdbc6e7302f91c15c4f1f07eaecf5062
topolov.s <topolov.s@ad1team.ru>
2019-08-11 01:16:51 +0300

    add dark theme

--- src/js/config/theme.js
+++ src/js/config/theme.js
@@ -1,5 +1,5 @@
-export const defaultTheme = 1;
+export const defaultTheme = 2;
 
-export const defaultThemes = [{ value: 1, label: 'themeDefault' }, { value: 2, label: 'themeDark' }];
+export const defaultThemes = [{ value: 1, label: 'theme1' }, { value: 2, label: 'theme2' }];
 
 export const megafon_cookie_name_theme = 'megafon_theme';
